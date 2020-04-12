cf6551e7397896b70bf41163a73bf959471da9ac
Vahid Næini <naeini.v@gmail.com>
2020-04-12 14:57:04 +0430

    Update prime_checker.py

--- prime/prime_checker.py
+++ prime/prime_checker.py
@@ -3,14 +3,22 @@ import time
 import math
 import random
 
+def printit(f):
+    def inner(*args, **kwargs):
+        print(f(*args, **kwargs))
+    return inner
 
-def is_prime_1(n:int) -> bool:
-    for i in range(2, n):
-        if n % i == 0:
+
+def trial_composite(a, d, n, s):
+    if pow(a, d, n) == 1:
+        return False
+    for i in range(s):
+        if pow(a, 2 ** i * d, n) == n - 1:
             return False
     return True
 
 
+@printit
 def is_prime_2(n:int) -> bool:
     for i in range(2, int(math.sqrt(n))):
         if n % i == 0:
@@ -18,22 +26,13 @@ def is_prime_2(n:int) -> bool:
     return True
 
 
-def is_prime_3(n: int) -> bool:
-    """
-    Miller-Rabin primality test.
-
-    A return value of False means n is certainly not prime. A return value of
-    True means n is very likely a prime.
-    """
-    if n != int(n):
-        return False
-    n = int(n)
-    # Miller-Rabin test for prime
+@printit
+def is_prime_3(n: int) -> str:
     if n == 0 or n == 1 or n == 4 or n == 6 or n == 8 or n == 9:
-        return False
+        return "0"
 
     if n == 2 or n == 3 or n == 5 or n == 7:
-        return True
+        return "1"
     s = 0
     d = n - 1
     while d % 2 == 0:
@@ -41,30 +40,21 @@ def is_prime_3(n: int) -> bool:
         s += 1
     assert (2 ** s * d == n - 1)
 
-    def trial_composite(a):
-        if pow(a, d, n) == 1:
-            return False
-        for i in range(s):
-            if pow(a, 2 ** i * d, n) == n - 1:
-                return False
-        return True
 
     for i in range(8):  # number of trials
         a = random.randrange(2, n)
-        if trial_composite(a):
-            return False
+        if trial_composite(a, d, n, s):
+            return "0"
 
-    return True
+    return "1"
 
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
     
     with open(file_name) as f:
-        numbers = [int(s) for s in f.read().split('\n') if s.strip()]
-        for number in numbers:            
-            check = is_prime_2(number) 
-            if check:
-                print(1)
-            else:
-                print(0)
+        numbers = [int(s) for s in f.read().strip().split('\n')]
+        
+    for number in numbers:
+        is_prime_2(number)
+            
